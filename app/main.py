817b7920c2d3d7901c1326d85abbeb01312e0131
Immanuel Dsouza <dsouzad681@gmail.com>
2026-01-06 11:17:39 +0530

    added specific files needed for render deployments

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 from app.api.endpoints import health 
+import os 
 
 app = FastAPI(title="POC")
 
 app.include_router(health.router, tags=['health'])
 
-@app.get("/")
-def root():
-    return {"message":"hi server."}
+# @app.get("/")
+# def root():
+#     return {"message":"hi server."}
+
+
+if __name__ == "__main__" :
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app",host="0.0.0.0",port=port,reload=False)
