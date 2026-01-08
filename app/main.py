5448cdacee4a4b8f8c9ce913bec552ea9d428197
Immanuel Dsouza <dsouzad681@gmail.com>
2026-01-08 12:51:53 +0530

    whatsapp get all expenses work in progress

--- app/main.py
+++ app/main.py
@@ -5,6 +5,9 @@ import os
 import json
 import requests
 from dotenv import load_dotenv
+import re
+from typing import List,Any,Optional,Dict
+from dataclasses import dataclass, asdict
 
 load_dotenv()
 
@@ -14,11 +17,157 @@ WHATSAPP_TOKEN:str = os.getenv("WHATSAPP_TOKEN","NO_TOKEN")# temp token or perma
 
 
 app = FastAPI(title="Facebook Webhook Server")
+BASE_URL:str = "https://exp-man.m.frappe.cloud"
+@dataclass
+class Expense:
+    name: str
+    description: Optional[str]
+    expense_amount: float
+    employee: str
+
+@dataclass
+class ExpenseResponse:
+    data: List[Expense]
+    
+    @classmethod
+    def from_dict(cls, data_dict: Dict[str, Any]) -> 'ExpenseResponse':
+        expenses = []
+
+        print("data dict in from_dict():")
+        print(data_dict)
+        for item in data_dict.get("data", []):
+            expense = Expense(
+                name=item.get("name", ""),
+                description=item.get("description"),
+                expense_amount=item.get("expense_amount", 0.0),
+                employee=item.get("employee", "")
+            )
+            expenses.append(expense)
+        return cls(data=expenses)
+    
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            "data": [asdict(expense) for expense in self.data]
+        }
+    
+    def to_json(self) -> str:
+        return json.dumps(self.to_dict(), indent=2, default=str)
+    
+    def serialize(self) -> str:
+        """Serialize to JSON string"""
+        return self.to_json()
+#---------------------------------------------------------------
+
+#------------------------helper--------------------------------
+def serialize_get_api_response(api_response:Dict[str,Any]):
+    rsp = ExpenseResponse.from_dict(api_response)
+    formatted_strings = []
+
+    for idx, expense in enumerate(rsp.data, 1):
+        expense_str = f"""Expense-{idx}
+                Name: {expense.name}
+                Description: {expense.description if expense.description else 'No description'}
+                Expense Amount: {expense.expense_amount}
+                Employee: {expense.employee}
+
+                """
+        formatted_strings.append(expense_str)
+    
+    return "".join(formatted_strings)
+
 
 def print_json(data, indent=2):
     """Pretty print JSON"""
     print(json.dumps(data, indent=indent))
 
+
+async def handle_add_expense(phone_number:str, message:str)->bool:
+    return True
+
+async def handle_get_user_expense(phone_number:str, message:str)->str :
+    endpoint:str = "api/resource/CTExpense"
+    headers:dict[str,str] = {
+        "Authorization" : "token 9822fb1487561f6:8c3f8d54fd1cade"
+    }
+
+    params:dict[str,str] = {
+        "fields":json.dumps(["name","description","expense_amount","employee"])
+    }
+
+    url:str = f"{BASE_URL}/{endpoint}"
+
+    # print("requested url is ", url)
+    try :
+        response =  requests.get(url=url,  headers=headers, params=params)
+        response.raise_for_status()
+        formatted_response =  serialize_get_api_response(response.json())
+        return formatted_response
+        # return response.json()
+    except requests.exceptions.RequestException as e :
+        print(f"Error fetching data: {e}")
+        return "Something went wrong. Please report this problem to us."
+    except Exception as e :
+        print(f"Something went wrong : {e}")
+        return "Something went wrong. Please report this problem to us."
+
+async def craft_message(phone_number:str, message:str) -> bool :
+    regex_pattern : str =  r'\b(hi|hello)\b'
+    bot_message:str = ''
+    result  = False
+    
+    thanku_regex_pattern = r'\b(thank\s*you|thanks|thanx|thnx|thx|ty)\b'
+
+    if re.search(regex_pattern, message, re.IGNORECASE):
+        bot_message = """
+            Hi there, Expense manager bot this side. How can I help you today.\n
+            Type in the number to perform the desired task.\n\n
+
+            1. Add expense
+            2. Get user expense
+
+            Note : Type in the number . For eg : 1 or 2.
+        """
+        result = await send_whatsapp_message(phone_number=phone_number, message=bot_message)
+
+    elif re.search(thanku_regex_pattern, message, re.IGNORECASE):
+        bot_message = """
+           Your Welcome. 
+           Thank you for using our service.
+        """
+        result = await send_whatsapp_message(phone_number=phone_number, message=bot_message)
+
+    else :
+    # check if the input is a valid numerical  choice or not.
+        if message.isdigit() : 
+            choice = int(message)
+
+            if (choice > 2 or choice < 1) :
+                bot_message = """
+                    Invalid choice provided. \n
+                    Kindly pick the valid choices again. 
+
+                    1. Add expense 
+                    2. Get user expense
+                """
+                result = await send_whatsapp_message(phone_number, "Invalid choice. Kindly choose either 1 or 2 as the choice.")
+                return result
+            
+
+            match choice:
+                case 1:
+                    bot_message = await handle_add_expense(phone_number=phone_number, message="Add expense api is not working now. Try later.")
+                    result = await send_whatsapp_message(phone_number=phone_number, message=bot_message)
+                    return result
+                case 2:
+                    bot_message =  await handle_get_user_expense(phone_number=phone_number, message=message)
+                    result = await send_whatsapp_message(phone_number=phone_number, message=bot_message)
+                    return result
+              
+        else :
+            bot_message = f"{message} is not a valid integer. It should be either 1 or 2. Kindly retry again."
+            result = await send_whatsapp_message(phone_number=phone_number, message=bot_message)
+
+    return result
 
 async def send_whatsapp_message(phone_number:str, message:str)->bool:
     result:bool = False
@@ -95,7 +244,8 @@ async def receive_webhook(request: Request):
 
         # want to make an api request here and send the status as success or failure.
         # your code goes here.
-        api_result = await send_whatsapp_message(phone_number, message)
+        api_result:str =  craft_message(phone_number=phone_number, message=message)
+        # api_result = await send_whatsapp_message(phone_number, message)
         
         # print_json(body)
     except Exception as e:
@@ -114,6 +264,10 @@ async def health_check():
     print("token from .env file is ", WHATSAPP_TOKEN)
     return {"status": "healthy", "service": "facebook-webhook"}
 
+@app.get("/testing_get_api")
+async def get_response(request:Request):
+    resp =  await  handle_get_user_expense("","")
+    return resp
 
 # For local development
 if __name__ == "__main__":
